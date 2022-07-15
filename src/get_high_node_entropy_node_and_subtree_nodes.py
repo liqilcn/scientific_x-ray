c09d5e7da01f16de2f711cba0010820e7241b5e4
qli_mac <1234@qq.com>
2022-07-15 18:31:17 +0800

    add pattern_statistics.py

--- src/get_high_node_entropy_node_and_subtree_nodes.py
+++ src/get_high_node_entropy_node_and_subtree_nodes.py
@@ -75,6 +75,7 @@ class MyNode:
     
 def get_subtree_nodes_recursion(NodeNow):
     # 递归遍历子树，获取子树的所有节点id
+    # 知识熵超越之后停止递归
     global subtree_nodes, leading_node_entropy
     for NodeLinked in NodeNow.ReturnBeCited():
         subtree_nodes.append(NodeLinked.ReturnID())
@@ -88,6 +89,21 @@ def get_subtree_nodes(NodeNow, p_id):
     leading_node_entropy = pid2node_entropy[p_id]
     subtree_nodes = []
     get_subtree_nodes_recursion(NodeNow)
+    return subtree_nodes
+
+def get_subtree_nodes_recursion_without_exceed(NodeNow):
+    # 忽略知识熵的超越效应
+    global subtree_nodes, leading_node_entropy
+    for NodeLinked in NodeNow.ReturnBeCited():
+        subtree_nodes.append(NodeLinked.ReturnID())
+#         print(len(NodeLinked.ReturnBeCited()))
+        get_subtree_nodes_recursion_without_exceed(NodeLinked)
+
+def get_subtree_nodes_without_exceed(NodeNow, p_id):
+    global subtree_nodes, leading_node_entropy
+    leading_node_entropy = pid2node_entropy[p_id]
+    subtree_nodes = []
+    get_subtree_nodes_recursion_without_exceed(NodeNow)
     return subtree_nodes
 
 def get_high_node_entropy_and_subtree_nodes(pid):
@@ -182,6 +198,53 @@ def get_high_node_entropy_and_subtree_nodes(pid):
     for p_id in selected_pids:
         NodeNow = id2node[str(p_id)]
         subtree_nodes = get_subtree_nodes(NodeNow, p_id)
+        selected_pid2subtree_nodes[p_id] = subtree_nodes
+    return selected_pid2subtree_nodes
+
+def get_high_node_entropy_and_subtree_nodes_without_exceed(pid):
+    # 树结构初始化
+    id2node = {}
+    NodeList = []
+    year_now = 2021
+    node_detail = json.load(open(f'../temp_files/skeleton_tree_by_year/{pid}/{year_now}', 'r'))
+    
+    tree_node_deep = json.load(open(f'../temp_files/tree_deep_by_year/{pid}/{year_now}', 'r'))
+    pid2depth = {}
+    for dp in tree_node_deep:
+        for p_id in tree_node_deep[dp]:
+            pid2depth[str(p_id)] = dp
+
+    for node in node_detail:
+        ID = str(node)
+        node = str(node)
+        label = node_detail[node]['label']
+        year = node_detail[node]['year']
+        NewNode = MyNode(ID,label,year)
+        id2node[node] = NewNode
+
+    for node in id2node:
+        for nd in node_detail[node]['cite']:
+            id2node[node].AppendCite(id2node[str(nd)])
+        for nd in node_detail[node]['becited']:
+            id2node[node].AppendBeCited(id2node[str(nd)])
+        NodeList.append(id2node[node])
+
+    # 获取所有点熵数值超过10的节点
+    global pid2node_entropy
+    pid2node_entropy = json.load(open(f'../temp_files/node_entropy_by_year/{pid}/{year_now}', 'r'))
+    selected_pids = []  # 所有点熵超过10的节点
+    for p_id, node_entropy in sorted(pid2node_entropy.items(), key=lambda item:item[1], reverse=True):
+        if p_id == pid:
+            continue
+        if node_entropy >= 10:
+            selected_pids.append(p_id)
+        else:
+            break
+    # 遍历子树，获取选定节点的子树中所有节点id
+    selected_pid2subtree_nodes = {}
+    for p_id in selected_pids:
+        NodeNow = id2node[str(p_id)]
+        subtree_nodes = get_subtree_nodes_without_exceed(NodeNow, p_id)
         selected_pid2subtree_nodes[p_id] = subtree_nodes
     return selected_pid2subtree_nodes
 
